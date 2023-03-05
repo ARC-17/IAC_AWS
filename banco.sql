e498cebe1921029e47b1f413535f3f6291763d70
arc-17 <ti.rarcanjo@gmail.com>
2023-03-05 11:01:04 -0300

    first commit

---
+++ banco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE clientes (
+    ClienteID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50),
+    Email varchar(50)
+);
